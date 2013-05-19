6e18becaa29840aa6b0e1198e301004af1557b63
Brian Greenacre <bgreenacre42@gmail.com>
2013-05-19 15:22:50 -0500

    Updates.

--- src/Wordpress/Api/Themes.php
+++ src/Wordpress/Api/Themes.php
@@ -1,5 +1,7 @@
 <?php namespace Wordpress\Api;
 
+use Goutte\Client;
+
 class Themes {
 
     /**
@@ -56,6 +58,45 @@ class Themes {
                 )
             )
             ->run();
+    }
+
+    public function getVersions($slug)
+    {
+        $url = sprintf('https://wordpress.org/extend/themes/%s/developers/', $slug);
+        $client = new Client();
+
+        $page = $client->request('GET', $url);
+        $versionsFiltered = $page->filter('a')->each(function($node, $i) use ($slug)
+        {
+            preg_match(
+                '#[^\s]+(' . preg_quote($slug) . ')\.?([0-9\.]+)\.zip$#',
+                $node->attr('href'),
+                $matches
+            );
+
+            if (count($matches) > 0)
+            {
+                return array(array_pop($matches), $matches[0]);
+            }
+            else
+            {
+                return false;
+            }
+        });
+
+        $versionsFiltered = array_filter($versionsFiltered, function($value)
+        {
+            return (false !== $value);
+        });
+
+        $versions = array();
+
+        foreach ($versionsFiltered as $version)
+        {
+            $versions[$version[0]] = $version[1];
+        }
+
+        return $versions;
     }
 
     public function getNew($page = 1)
